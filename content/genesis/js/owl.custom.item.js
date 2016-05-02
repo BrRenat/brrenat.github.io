152fbf793e22720f7d0fecdbd18d814b5952bd80
Renat Berezovsky <renat0014@gmail.com>
2016-05-02 15:39:49 +0300

    genesis complete

--- content/genesis/js/owl.custom.item.js
+++ content/genesis/js/owl.custom.item.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
 		if ($(window).width() <= '540'){
 			     	$(".owl-carousel1").owlCarousel({
-						items: 2
+						items: 2,
+						loop: false
 					});
 					$(".owl-carousel2").owlCarousel({
 						items: 1
 						});
 			     } else {
 			     	$(".owl-carousel1").owlCarousel({
-						items: 4
+						items: 4,
+						loop: false
 					});
 					$(".owl-carousel2").owlCarousel({
 						items: 2
--- content/genesis/mail.php
+++ content/genesis/mail.php
@@ -2,17 +2,14 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['name'])) {$name = $_POST['name'];}
     if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
-    if (isset($_POST['email'])) {$text = $_POST['email'];}
-    if (isset($_POST['formData'])) {$formData = $_POST['formData'];}
-
+    if (isset($_POST['email'])) {$email = $_POST['email'];}
+    if (isset($_POST['title'])) {$title = $_POST['title'];}
+    if (isset($_POST['text'])) {$text = $_POST['text'];}
+    if (isset($_POST['service'])) {$service = $_POST['service'];}
     $to = "renat0014@gmail.com"; /*Укажите адрес, на который должно приходить письмо*/
-    $sendfrom   = "Genesis media group"; /*Укажите адрес, с которого будет приходить письмо, можно не настоящий, нужно для формирования заголовка письма*/
+    $sendfrom   = "Genesis media group";
     $headers  = "From: " . strip_tags($sendfrom) . "\r\n";
-    $headers .= "Reply-To: ". strip_tags($sendfrom) . "\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type:multipart/mixed;charset=utf-8 \r\n";
-    $subject = "$formData";
-    $message = "$formData Имя пославшего: $name Телефон: $phone Почта: $email";
+    $message = "Имя пославшего: $name Телефон: $phone Почта: $email Услуга: $service Тема сообщения: $title Текст сообщения: $text";
 
 
     $send = mail ($to, $subject, $message, $headers);
