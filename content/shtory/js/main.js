8f3eccba4ee97ef4f91ee41bdd9d15921a32a51e
Renat Berezovsky <renat0014@gmail.com>
2016-06-04 22:34:46 +0300

    fix shtory lp

---
+++ content/shtory/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	  $('.owl-carousel').owlCarousel({
+						items: 1,
+						nav:true,
+						navText: ['','<button class="custom-btn owl-next">Следующий проект</button>'],
+						loop: true
+					});
+	});
