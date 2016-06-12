6056e26c2a5e033eb7d5ed46cf912d41c7448761
Renat Berezovsky <renat0014@gmail.com>
2016-06-12 13:01:29 +0300

    scroll effect

--- content/scroll/js/main.js
+++ content/scroll/js/main.js
@@ -1,34 +1,72 @@
 $(document).ready(function(){
-Zepto(".main").onePageScroll({
-	
-   sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-   easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", 
-                                    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-   animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
-   animationDelay: 300,
-   pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-   updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-   beforeMove: function(index) {
-      currentElement = $(document).find("[data-index='" + (index - 1) + "']").find('.wrap');
-      nextElement = $(document).find("[data-index='" + index + "']").find('.wrap');
-      currentElement.animate({scale: 0.9}, 300, 'ease-out');
-      nextElement.animate({scale: 0.9}, 0, 'ease-out');
+var x;
+   Zepto(".main").onePageScroll({
 
-      fisrtAct = $(".onepage-pagination").find("[data-index='" + index + "']");
-      fisrtTop = fisrtAct.offset().top;
-      // $(".active-pagination").animate("animationPagination", {duration:800, easing: 'ease-out'});
-      $(".active-pagination").animate({translateY: (fisrtTop + 0.5) + 'px'}, 0, 'ease-out');
-      console.log(fisrtTop);
-   },                               // This option accepts a callback function. The function will be called before the page moves.
-   afterMove: function(index) {
-   	currentElement.animate({scale: 1}, 00, 'ease-out');
-   	nextElement.animate({scale: 1}, 300, 'ease-out');
+      sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+      easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", 
+                                       // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+      animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+      animationDelay: 300,
+      pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+      updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+      beforeMove: function(index) {
+         currentElement = $(document).find("[data-index='" + (index - 1) + "']").find('.wrap');
+         nextElement = $(document).find("[data-index='" + index + "']").find('.wrap');
+         currentElement.animate({scale: 0.9}, 300, 'ease-out');
+         nextElement.animate({scale: 0.9}, 0, 'ease-out');
+
+         if ( event.deltaY > 0 ) {
+            fisrtAct = $(".onepage-pagination").find("[data-index='" + (index - 1) + "']");
+            fisrtTop = fisrtAct.offset().top;
+            secondAct = $(".onepage-pagination").find("[data-index='" + index + "']");
+            secondTop = secondAct.offset().top;
          
-   },                               // This option accepts a callback function. The function will be called after the page moves.
-   loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-   keyboard: true,                  // You can activate the keyboard controls
-   responsiveFallback: false        // You can fallback to normal page scroll by defining the width of the browser in which
-                                    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever 
-                                    // the browser's width is less than 600, the fallback will kick in.
+            $(".active-pagination").queueAnim([
+              [ { 'translateY': (fisrtTop + (secondTop - fisrtTop) / 4) + 'px', 'scale': '0.5' }, 500, 'ease-out' ],
+              [ { 'translateY': (fisrtTop + (secondTop - fisrtTop) / 4 * 3) + 'px', 'scale': '0.5' }, 300, 'ease-out' ],
+              [ { 'translateY': secondTop + 'px', 'scale': '1' }, 500, 'ease-out' ]
+            ]);
+         } else {
+            fisrtAct = $(".onepage-pagination").find("[data-index='" + index + "']");
+            fisrtTop = fisrtAct.offset().top;
+            secondAct = $(".onepage-pagination").find("[data-index='" + (index + 1) + "']");
+            secondTop = secondAct.offset().top;
+
+            $(".active-pagination").queueAnim([
+               [ { 'translateY': (fisrtTop + (secondTop - fisrtTop) / 4 * 3) + 'px', 'scale': '0.5' }, 500, 'ease-out' ],
+               [ { 'translateY': (fisrtTop + (secondTop - fisrtTop) / 4) + 'px', 'scale': '0.5' }, 300, 'ease-out' ],
+               [ { 'translateY': fisrtTop + 'px', 'scale': '1' }, 500, 'ease-out' ]
+            ]);
+         };
+         // $(".active-pagination").animate("animationPagination", {duration:800, easing: 'ease-out'});
+         
+      },                               // This option accepts a callback function. The function will be called before the page moves.
+      afterMove: function(index) {
+      	currentElement.animate({scale: 1}, 00, 'ease-out');
+      	nextElement.animate({scale: 1}, 300, 'ease-out');
+      },                               // This option accepts a callback function. The function will be called after the page moves.
+      loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+      keyboard: true,                  // You can activate the keyboard controls
+      responsiveFallback: false        // You can fallback to normal page scroll by defining the width of the browser in which
+                                       // you want the responsive fallback to be triggered. For example, set this to 600 and whenever 
+                                       // the browser's width is less than 600, the fallback will kick in.
+   });
 });
-});
+
+$.fn.queueAnim = function (steps, callback) {
+  var $selector = this;
+
+  function iterator(step) {
+    step.push(iterate);
+    $selector.animate.apply($selector, step); 
+  }
+
+  function iterate() {
+    if (!steps.length) return callback && callback();
+    
+    var step = steps.shift();
+    iterator(step);
+  }
+
+  iterate();
+}
